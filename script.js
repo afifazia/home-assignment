4b8a3cdac00ebc1dab2ee950fa7f4b6125747f59
afifazia2007 <afifazia07@gmail.com>
2024-11-30 12:05:57 +0500

    Conditions

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let FirstName="afifa";
-let LastName="zia";
-let Qualification="HDSE";
+var Name = prompt("Enter your name");
+if(Name == "Afifa"){
+    document.write( "<h1 style='color:green'>My name is "+Name+"</h1>");
+} else {
+    document.write("Error");
+}
 
-document,write( "<h1 style='color:green'>"+FirstName+"</h1>");
+var FName = prompt("Enter your father's name");
+if(FName == "Zia"){
+    document.write( "<h2 style='color:blue'>My father's name is "+FName+"</h2>");
+} else {
+    document.write("Error");
+}
+
+var Qual = prompt("Enter your father's name");
+if(Qual == "Matriculation"){
+    document.write( "<h3 style='color:red'>My qualification is "+Qual+"</h3>");
+} else {
+    document.write("Error");
+}
