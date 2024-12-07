aaee20b3000daea7ea7adc533311c93f27f04d6c
afifazia2007 <afifazia07@gmail.com>
2024-12-07 14:26:34 +0500

    Daraz

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var Name = prompt("Enter your name");
-if(Name == "Afifa"){
-    document.write( "<h1 style='color:green'>My name is "+Name+"</h1>");
+var Desc = prompt("Enter your Desc");
+if(Desc == "Afifa"){
+    document.write( "<h1 style='color:green'>My Desc is "+Desc+"</h1>");
 } else {
     document.write("Error");
 }
 
-var FName = prompt("Enter your father's name");
-if(FName == "Zia"){
-    document.write( "<h2 style='color:blue'>My father's name is "+FName+"</h2>");
+var FDesc = prompt("Enter your father's Desc");
+if(FDesc == "Zia"){
+    document.write( "<h2 style='color:blue'>My father's Desc is "+FDesc+"</h2>");
 } else {
     document.write("Error");
 }
 
-var Qual = prompt("Enter your father's name");
+var Qual = prompt("Enter your father's Desc");
 if(Qual == "Matriculation"){
     document.write( "<h3 style='color:red'>My qualification is "+Qual+"</h3>");
 } else {
